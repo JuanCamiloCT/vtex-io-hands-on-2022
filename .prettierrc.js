0af76e1775298ef6fee99a36131817166d88cdbf
Camilo Torres <juan-ca555@hotmail.com>
2022-07-12 02:41:23 -0500

    feat(upload-project)

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // Add semicolon at the end of simples statements
+    semi: true,
+
+    // Include parentheses around a sole arrow function parameter.
+    arrowParens: "always",
+
+    // Specify the number of spaces per indentation-level.
+    tabWidth: 4,
+};
